44f62b52621b698213a4babcf7c7d66c4bd4fd6b
ChiaLing <hochialing91@gmail.com>
2020-03-17 12:19:06 +0100

    set basename at Router

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ class App extends Component {
     return (
       <div>
         <div className="App">
-          <Router basename="/" /*history={history}*/>
+          <Router basename="" /*history={history}*/>
             <Header />
 
             <Switch>
